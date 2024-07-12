b064d9b678ef6ee3e92f2d97c2f91f4ec77d5a53
fabriciodasilvapires <fabriciopires@discente.ufg.br>
2024-07-12 13:42:13 -0300

    ATIVIDADES - FUNDAMENTOS DE PROGRAMAÇÃO BACK-END

---
+++ Pratica_04_Spring_Mvc_Crud/src/main/java/br/inf/ufg/backend/Pratica_04_Spring_Mvc_Crud/model/Produto.java
@@ -0,0 +1,42 @@
+package br.inf.ufg.backend.atividade_pratica_04_spring_mvc_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.List;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
