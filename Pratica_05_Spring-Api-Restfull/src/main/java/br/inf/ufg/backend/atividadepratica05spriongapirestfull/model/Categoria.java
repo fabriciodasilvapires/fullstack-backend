c06a92f0e854c7358718296c2f60d0f42d6483ff
fabriciodasilvapires <fabriciopires@discente.ufg.br>
2024-07-12 15:15:15 -0300

    ATIVIDADES - FUNDAMENTOS DE PROGRAMAÇÃO BACK-END

---
+++ Pratica_05_Spring-Api-Restfull/src/main/java/br/inf/ufg/backend/atividadepratica05spriongapirestfull/model/Categoria.java
@@ -0,0 +1,36 @@
+package br.inf.ufg.backend.atividadepratica05springapirestfull.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
